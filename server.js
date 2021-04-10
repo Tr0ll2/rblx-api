382eff8dd9fc38b53665d26f6739f0d2a8346e17
Joshua Torres <josh@Joshuas-Air.lan>
2021-04-09 21:22:36 -0400

    Added gif capability

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const isImage = require('is-image-url');
 const isUrl = require('is-absolute-url');
-const getPixels = require('get-pixels');
+const getPixels = require('get-pixels-frame-info-update');
 
 app.engine('html', require("ejs").renderFile);
 
@@ -27,89 +27,87 @@ app.get("/ping", (req,res)=>{
 
 //path = "/pixel/data" and/or /pixel/data?url=
 pixel.get("/data", (req,res)=>{
-  var url = req.query.url;
-  if (url){
+	var url = req.query.url;
+  	if (url){
 		var end = endofURL(url)
-    console.log(url)
-    if (isUrl(url)) {
-      console.log("URL, ending in " + end + ", is real")
-      if (isImage(url)) {
-        console.log("URL, ending in " + end + ", is an image")
-        getPixels(url, function(err, pixels, reader) {
-  				if (err) {
-  					console.log(err)
-  					return
-  				} else {
-  					var array = [];
-  					if(pixels.shape.length < 4){
-  						var width = pixels.shape[0];
-  						var height = pixels.shape[1];
-              var frame = [];
-  						for(var y = 0;y < height;y++){
-  							var row = [];
-  							for(var x = 0;x < width;x++){
-  								var r = pixels.get(x,y,0);
-  								var g = pixels.get(x,y,1);
-  								var b = pixels.get(x,y,2);
-  								var a = 127 - (pixels.get(x,y,3)*(127/255));
-  								var pixel = [r,g,b,a];
-  								row.push(pixel);
-  							}
-  							frame.push(row);
-  						}
-              array.push(frame)
-  					}/*else{
-  						var frames = pixels.shape[0];
-  						var width = pixels.shape[1];
-  						var height = pixels.shape[2];
-              var state = [];
-  						for(var z = 0;z < frames;z++){ // z represents the specific frame of a gif
-  							var frame = [];
-                var frameInfo = reader.frameInfo(z);
-                var dispose = frameInfo.disposal;
+    	console.log(url)
+    	if (isUrl(url)) {
+			console.log("URL, ending in " + end + ", is real")
+      		if (isImage(url)) {
+    			console.log("URL, ending in " + end + ", is an image")
+        		getPixels(url, function(err, pixels, frameInfo) {
+  					if (err) {
+  						console.log(err)
+  						return
+  					} else {
+  						var array = [];
+  						if(pixels.shape.length < 4){
+  							var width = pixels.shape[0];
+  							var height = pixels.shape[1];
+              				var frame = [];
   							for(var y = 0;y < height;y++){
-                  state[y] = state[y] || [];
   								var row = [];
   								for(var x = 0;x < width;x++){
-                    state[y][x] = state[y][x] || [0,0,0,127];
-  									var r = pixels.get(z,x,y,0);
-  									var g = pixels.get(z,x,y,1);
-  									var b = pixels.get(z,x,y,2);
-  									var a = 127 - (pixels.get(z,x,y,3)*(127/255));
-  									var pixel;
-                    var empty = a === 127;
-  									if(empty){
-  										pixel = state[y][x].slice();
-  									}else{
-  										pixel = [r,g,b,a];
-  									}
+  									var r = pixels.get(x,y,0);
+  									var g = pixels.get(x,y,1);
+  									var b = pixels.get(x,y,2);
+  									var a = 127 - (pixels.get(x,y,3)*(127/255));
+  									var pixel = [r,g,b,a];
   									row.push(pixel);
-                    if(dispose <= 1 && !empty){ //Dispose 0 (Unspecified) and Dispose 1 (Do Not Dispose) mean save the frame to the state
-                      state[y][x] = pixel.slice();
-                    }else if(dispose === 2){ //Dispose 2 (Restore To Background) means nuke the state
-                      state[y][x] = [0,0,0,127];
-                    } //Dispose 3 (Restore to Previous) means do not save the frame to the state, so no changes to the state are needed
   								}
   								frame.push(row);
   							}
-  							frame.push(frameInfo.delay*10); //Times 10 because GIF protocols specify delay in terms of 100ths of a second, not milliseconds (which are 1000ths)
-  							array.push(frame);
+              				array.push(frame)
+  						} else {
+							var frames = pixels.shape[0];
+							var width = pixels.shape[1];
+							var height = pixels.shape[2];
+							var state = [];
+							for(var z = 0; z < frames; z++){ // z represents the specific frame of a gif
+  								var frame = [];
+                				var dispose = frameInfo.disposal;
+  								for(var y = 0; y < height; y++){
+                 					state[y] = state[y] || [];
+  									var row = [];
+  									for(var x = 0; x < width; x++){
+                    					state[y][x] = state[y][x] || [0,0,0,127];
+										var r = pixels.get(z,x,y,0);
+										var g = pixels.get(z,x,y,1);
+										var b = pixels.get(z,x,y,2);
+										var a = 127 - (pixels.get(z,x,y,3)*(127/255));
+										var pixel;
+										var empty = a === 127;
+										if (empty){
+											pixel = state[y][x].slice();
+										}else{
+											pixel = [r,g,b,a];
+										}
+  										row.push(pixel);
+										if (dispose <= 1 && !empty) { //Dispose 0 (Unspecified) and Dispose 1 (Do Not Dispose) mean save the frame to the state
+											state[y][x] = pixel.slice();
+										} else if (dispose === 2) { //Dispose 2 (Restore To Background) means nuke the state
+											state[y][x] = [0,0,0,127];
+										} //Dispose 3 (Restore to Previous) means do not save the frame to the state, so no changes to the state are needed
+  									}
+  									frame.push(row);
+  								}
+  								//frame.push(frameInfo.delay); //Times 10 because GIF protocols specify delay in terms of 100ths of a second, not milliseconds (which are 1000ths)
+  								array.push(frame);
+  							}
   						}
-  					}*/
-  					var json = JSON.stringify(array);
-  					res.send(json);
-  				}
-  			});
-      } else {
-        console.log("URL, ending in " + end + ", is not an image")
-      }
-    } else {
-      console.log("URL, ending in " + end + ", is false")
-
-    }
-  } else {
-    res.send("No URL given. The params should be /pixel/data?url=(URL)")
-  }
+  						var json = JSON.stringify(array);
+  						res.send(json);
+  					}
+  				});
+    		} else {
+    			console.log("URL, ending in " + end + ", is not an image")
+    		}
+		} else {
+      		console.log("URL, ending in " + end + ", is false")
+		}
+	} else {
+		res.send("No URL given. The params should be /pixel/data?url=(URL)")
+  	}
 })
 
 app.listen(port, ()=>console.log("Working"))
